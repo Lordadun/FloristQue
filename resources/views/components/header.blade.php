1d79b0a66bb6838a3378bdb413cb942f75f008b5
haidar <muhammadhaidar702@gmail.com>
2026-04-11 11:27:59 +0700

    feat: login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,6 +2,18 @@
                dark:from-gray-800 dark:to-gray-700
                flex justify-between items-center px-6 py-4 shadow-md">
 
+    <div class="flex items-center gap-3">
+
+        <span class="text-white text-sm">
+            {{ session('user') }}
+        </span>
+
+        <a href="/logout" class="bg-white text-black px-3 py-1 rounded-lg text-sm">
+            Logout
+        </a>
+
+    </div>
+
     <!-- Title -->
     <h1 class="text-2xl font-bold text-white tracking-wide">
         🌸 FloristQue
