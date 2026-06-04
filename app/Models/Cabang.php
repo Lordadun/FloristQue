6b5efa4c36b1d9673be27772e7a3fdf0b5987736
Nurlela1203 <nurlela1203@icloud.com>
2026-06-04 17:59:06 +0700

    Update Model Cabang

--- app/Models/Cabang.php
+++ app/Models/Cabang.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cabang extends Model
 {
-    //
+    protected $fillable = [
+        'nama_cabang',
+        'alamat',
+        'kota',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function stokBarangs()
+    {
+        return $this->hasMany(StokBarang::class);
+    }
+
+    public function transaksis()
+    {
+        return $this->hasMany(Transaksi::class);
+    }
 }
