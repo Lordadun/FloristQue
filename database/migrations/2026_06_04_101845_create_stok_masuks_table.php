26ce7a8478b4d8215cf8495fc9af356934cf3027
Nurlela1203 <nurlela1203@icloud.com>
2026-06-04 17:55:18 +0700

    Migrasi Table Stok Masuk

--- database/migrations/2026_06_04_101845_create_stok_masuks_table.php
+++ database/migrations/2026_06_04_101845_create_stok_masuks_table.php
@@ -12,9 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('stok_masuks', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        $table->id();
+        $table->foreignId('cabang_id')->constrained('cabangs')->cascadeOnDelete();
+        $table->foreignId('produk_id')->constrained('produks')->cascadeOnDelete();
+        $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        $table->integer('jumlah');
+        $table->date('tanggal');
+        $table->text('keterangan')->nullable();
+        $table->timestamps();
+    });
     }
 
     /**
