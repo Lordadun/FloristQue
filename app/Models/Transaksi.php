b40a4bc9550a308831d6e8731c1a1b4196491e8c
Nurlela1203 <nurlela1203@icloud.com>
2026-06-04 18:01:30 +0700

    Update Model Transaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
-    //
+    protected $fillable = [
+        'kode_transaksi',
+        'cabang_id',
+        'kasir_id',
+        'total',
+        'bayar',
+        'kembalian',
+        'tanggal_transaksi',
+    ];
+
+    public function cabang()
+    {
+        return $this->belongsTo(Cabang::class);
+    }
+
+    public function kasir()
+    {
+        return $this->belongsTo(User::class, 'kasir_id');
+    }
+
+    public function detailTransaksis()
+    {
+        return $this->hasMany(DetailTransaksi::class);
+    }
 }
